9ee6e882f4e5d8494857971a1e1e7ead33fe7a9f
MuhammadTalha15 <talhaz5518@gmail.com>
2025-06-10 22:53:45 +0500

    Server Data

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+app.use(cors())
+
 app.get('/', (req, res) => {
     res.send('Server Initiallized | Ubuntu 24.04 Noble Numbat');
+});
+
+let dataArray = [];
+
+app.post('/sendData', async (req, res) => {
+    const todoData = await req.body;
+
+    if (!todoData) {
+        return res.status(400).json({ status: 'Failed to Receive' });
+    }
+
+    dataArray.push(todoData);
+    console.log(dataArray)
+    return res.status(200).json({ message: 'Data Sent' })
+});
+
+app.delete('/deleteData/:id', (req, res) => {
+    const idToDelete = parseInt(req.params.id);
+    const index = dataArray.findIndex(item => item.id === idToDelete);
+
+    if (index == -1) {
+        return res.status(404).json({ message: 'Item not Deleted' });
+    }
+
+    dataArray.splice(index, 1);
+    console.log('Remaing Items', dataArray);
+    return res.status(200).json({ message: 'Item Deleted' });
 })
 
 app.listen(PORT, () => {
